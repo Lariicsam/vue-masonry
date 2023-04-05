12dbb6002dfb2ad12f6da540c0c0003f6a62c640
Larissa <larissa@latouraineinc.com>
2023-04-05 16:40:54 -0600

    Initialize project

---
+++ src/App.vue
@@ -0,0 +1,11 @@
+<script setup>
+import MasonryCard from "./components/MasonryCard.vue";
+
+</script>
+
+<template>
+<masonry-card></masonry-card>
+
+
+</template>
+
---
+++ src/components/MasonryCard.vue
@@ -0,0 +1,28 @@
+<script setup>
+defineProps({
+  msg: {
+    type: String,
+    required: true,
+  },
+});
+</script>
+<template>
+  <div
+    class="masonry-item"
+    style="
+      background-image: url('https://nacms-production.s3.amazonaws.com/50264/out..jpeg');
+    "
+  >
+    <div class="masonry-overlay">
+      <i class="masonry-fav-alone"></i>
+      <i class="masonry-edit"></i>
+      <strong>{{ $i }}</strong>
+      Alexcoal, Drawn By Alberto Vargas, Glamourous, Elegant, Sophisticated, Tar
+      Vintage, Pin Up, Alluring, Seductive, [ tarot card ]!!!!!, black hair,
+      full fac...
+    </div>
+    <span class="masonry-counter hide"
+      >6840<i class="masonry-fav-filled"></i
+    ></span>
+  </div>
+</template>
