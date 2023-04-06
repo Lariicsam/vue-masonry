c68d7da9420854b2f255dfe709d8e705976615d1
Larissa <larissa@latouraineinc.com>
2023-04-06 13:57:40 -0600

    Composition API: Computed

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -8,44 +8,50 @@
       <div class="btn" @click="decreaseCounter(2)">--</div>
       <div class="btn" @click="decreaseCounter(1)">-</div>
       <strong class="counter">{{ counterData.count }}</strong>
-      <div class="btn" @click="increaseCounter(1, $event)">+</div>
+      <div class="btn" @click="increaseCounter(1)">+</div>
       <div class="btn" @click="increaseCounter(2)">++</div>
     </div>
+
     <div class="irow center">
-      <div class="edit">
-        <h4>Edit counter title:</h4>
-        <input v-model="counterData.title" type="text" />
+      <p>This counter is {{ oddOrEven }}</p>
+    </div>
+    <div class="irow center">
+      <div class="icol md">
+        <h4 class="edit">Edit counter title:</h4>
+        <input v-model="counterData.title" type="text" class="edit-input" />
       </div>
     </div>
   </div>
 </template>
 
 <script>
-import { ref, reactive } from "vue";
+import { reactive, computed } from "vue";
 export default {
   setup() {
     const myNonReactiveData = "My static Non reactive Title";
-    const counter = ref(0),
-      counterTitle = ref("My Counter");
 
-    const increaseCounter = (amount, evnt) => {
-      console.log('evnt',evnt);
+    const oddOrEven = computed(() => {
+      if (counterData.count % 2 === 0) {
+        return "even";
+      } else {
+        return "odd";
+      }
+    });
+
+    const increaseCounter = (amount) => {
       counterData.count += amount;
     };
-
     const counterData = reactive({ count: 0, title: "My Title" });
-
     const decreaseCounter = (amount) => {
       counterData.count -= amount;
     };
 
     return {
       myNonReactiveData,
-      counter,
-      counterTitle,
       increaseCounter,
       decreaseCounter,
       counterData,
+      oddOrEven
     };
   },
 };
@@ -61,9 +67,20 @@ export default {
     cursor: pointer;
   }
 
-  .edit,
-  .edit > input {
-    color: darkgreen;
+  .edit {
+    font-size: 32px;
+    text-align: center;
+  }
+
+  .edit-input {
+    border: 1px solid azure;
+    background-color: white;
+    border-radius: 16px;
+    padding: 8px 0;
+    font-weight: lighter;
+    font-size: 24px;
+    color: green;
+    text-align: center;
   }
 </style>
  
